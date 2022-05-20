e87b551663ddcd7aeafd4bc5ebc1dc62d7c1563a
Nikolay Gelemerov <nikolaygelemerov@gmail.com>
2022-05-20 18:30:52 +0300

    Add `Autocomplete` `List` component

---
+++ src/ui/Input/Autocomplete/components/List/List.tsx
@@ -0,0 +1,92 @@
+import { createRef, FC, memo, RefObject, useMemo, useState } from 'react';
+import Scroll from 'react-scroll-component';
+
+import { usePrevious, useUpdate, useUpdateOnly } from '@rounik/react-custom-hooks';
+
+import { ListProps } from '../../types';
+
+import styles from './List.scss';
+
+export const List = <T,>({
+  dataTest,
+  id,
+  list,
+  multi,
+  renderOption,
+  rowsToDisplay = 4
+}: ListProps<T>) => {
+  const [style, setStyle] = useState({ height: 0, maxHeight: 0 });
+  const [listToRender, setListToRender] = useState(list);
+
+  const [rowRefs, setRowRefs] = useState<RefObject<HTMLLIElement>[]>([]);
+
+  const prevList = usePrevious(list);
+
+  const scrollerOptions = useMemo(() => {
+    return {
+      className: styles.ScrollerContainer,
+      direction: 'vertical',
+      height: style.maxHeight,
+      scrollerClass: styles.Scroller
+    };
+  }, [style.maxHeight]);
+
+  const options = useMemo(() => {
+    return listToRender.map((item, index) => {
+      return rowRefs[index] ? renderOption({ item, ref: rowRefs[index] }) : null;
+    });
+  }, [listToRender, renderOption, rowRefs]);
+
+  useUpdate(() => {
+    setRowRefs(listToRender.map(() => createRef()));
+  }, [listToRender]);
+
+  useUpdate(() => {
+    const rowHeight = (rowRefs && rowRefs[0]?.current?.clientHeight) ?? null;
+
+    setStyle((prevStyle) => ({
+      ...prevStyle,
+      height: rowHeight !== null ? list.length * rowHeight : 0,
+      ...(rowHeight !== null && prevStyle.maxHeight === 0
+        ? { maxHeight: rowsToDisplay * rowHeight }
+        : {})
+    }));
+  }, [rowRefs, list]);
+
+  useUpdateOnly(() => {
+    if (list.length === 0) {
+      setStyle((prevStyle) => ({ ...prevStyle, height: 0 }));
+    } else if (list.length >= prevList.length) {
+      setListToRender(list);
+    } else {
+      setListToRender(prevList);
+    }
+  }, [list, prevList]);
+
+  return (
+    <ul
+      aria-labelledby={`${id}-label`}
+      {...(multi ? { 'aria-multiselectable': true } : {})}
+      className={styles.List}
+      data-test={`${dataTest}-listbox`}
+      id={`${id}-listbox`}
+      onTransitionEnd={() => {
+        setStyle((prevStyle) => ({
+          ...prevStyle
+          // Remove native scroll
+          // overflowY: list.length > rowsToDisplay ? 'auto' : 'hidden'
+        }));
+
+        if (list.length < prevList.length) {
+          setListToRender(list);
+        }
+      }}
+      role="listbox"
+      style={style}
+    >
+      <Scroll {...scrollerOptions}>{options}</Scroll>
+    </ul>
+  );
+};
+
+export default memo(List);
--- src/ui/Input/Autocomplete/components/Option/Option.tsx
+++ src/ui/Input/Autocomplete/components/Option/Option.tsx
@@ -1,4 +1,4 @@
-import { FC, Fragment, memo, useCallback, useMemo, useRef } from 'react';
+import { forwardRef, Fragment, memo, RefObject, useCallback, useMemo } from 'react';
 
 import { useClass, useUpdate } from '@rounik/react-custom-hooks';
 
@@ -8,70 +8,70 @@ import { OptionProps } from '../../types';
 
 import styles from './Option.scss';
 
-export const Option: FC<OptionProps> = memo(({ className, dataTest, id, text, substitutes }) => {
-  const { close, focused, multi, opened, select, selected } = useComboBox();
+export const Option = memo(
+  forwardRef<HTMLLIElement, OptionProps>(({ className, dataTest, id, text, substitutes }, ref) => {
+    const { close, focused, multi, opened, select, selected } = useComboBox();
 
-  const { translate } = useTranslation();
+    const { translate } = useTranslation();
 
-  const element = useRef<HTMLLIElement>(null);
+    const label = useMemo(() => {
+      let translation = translate(text, substitutes);
 
-  const label = useMemo(() => {
-    let translation = translate(text, substitutes);
+      if (Array.isArray(translation)) {
+        translation = translation.map((el, index) => {
+          return <Fragment key={index}>{el}</Fragment>;
+        });
+      }
 
-    if (Array.isArray(translation)) {
-      translation = translation.map((el, index) => {
-        return <Fragment key={index}>{el}</Fragment>;
-      });
-    }
+      return translation;
+    }, [substitutes, text, translate]);
 
-    return translation;
-  }, [substitutes, text, translate]);
+    const isSelected = useMemo(() => {
+      return selected.includes(id);
+    }, [selected, id]);
 
-  const isSelected = useMemo(() => {
-    return selected.includes(id);
-  }, [selected, id]);
+    useUpdate(() => {
+      if (opened && id === focused) {
+        (ref as RefObject<HTMLLIElement>).current?.focus();
+      }
+    }, [opened, focused]);
 
-  useUpdate(() => {
-    if (opened && id === focused) {
-      element.current?.focus();
-    }
-  }, [opened, focused]);
+    return (
+      <li
+        tabIndex={-1}
+        aria-selected={isSelected}
+        className={useClass(
+          [styles.Container, isSelected && styles.Selected, className],
+          [className, isSelected]
+        )}
+        data-test={`${dataTest}-option`}
+        id={`${id}-option`}
+        onClick={useCallback(() => {
+          select(id);
 
-  return (
-    <li
-      tabIndex={-1}
-      aria-selected={isSelected}
-      className={useClass(
-        [styles.Container, isSelected && styles.Selected, className],
-        [className, isSelected]
-      )}
-      data-test={`${dataTest}-option`}
-      id={`${id}-option`}
-      onClick={useCallback(() => {
-        select(id);
-
-        if (!multi) {
-          close();
-        }
-      }, [close, id, multi, select])}
-      onKeyUp={useCallback(
-        (event) => {
-          if (event.code === 'Enter') {
-            select(id);
-
-            if (!multi) {
-              close();
-            }
+          if (!multi) {
+            close();
           }
-        },
-        [close, id, multi, select]
-      )}
-      ref={element}
-      role="option"
-    >
-      {label}
-    </li>
-  );
-});
+        }, [close, id, multi, select])}
+        onKeyUp={useCallback(
+          (event) => {
+            if (event.code === 'Enter') {
+              select(id);
+
+              if (!multi) {
+                close();
+              }
+            }
+          },
+          [close, id, multi, select]
+        )}
+        ref={ref}
+        role="option"
+      >
+        {label}
+      </li>
+    );
+  })
+);
 
 Option.displayName = 'Option';
