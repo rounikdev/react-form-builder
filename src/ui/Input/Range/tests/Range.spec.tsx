b847633dfc2f50b4c0405e2ee85ff6df8db4ff98
Mechoshi Puhanaga <mechokio@gmail.com>
2022-06-02 17:47:48 +0300

    Setup Range cypress tests

--- src/ui/Input/Range/tests/Range.spec.tsx
+++ src/ui/Input/Range/tests/Range.spec.tsx
@@ -1,3 +1,18 @@
+import { mount } from '@cypress/react';
+import { composeStory } from '@storybook/testing-react';
+
+import Meta, { RangeDemo } from '../stories/Range.stories';
+
+const RangeStory = composeStory(RangeDemo, Meta);
+
+const content = (
+  <div style={{ margin: '5rem 0 5rem 20rem' }}>
+    <RangeStory />
+  </div>
+);
+
 describe('Range', () => {
-  it('TODO', () => {});
+  it('TODO', () => {
+    mount(content);
+  });
 });
